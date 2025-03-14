ef0f54789771237aca3721f207105bfe5d26dbfe
nathantran183 <phuctd1803@gmail.com>
2025-03-15 00:30:27 +0800

    Update DbContext

--- src/MoneyMaster.Database/MoneyMasterContext.cs
+++ src/MoneyMaster.Database/MoneyMasterContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MoneyMaster.Database.Configurations;
-using MoneyMaster.Database.Models;
+using MoneyMaster.Database.Entities;
 
 namespace MoneyMaster.Database
 {
@@ -11,12 +11,18 @@ namespace MoneyMaster.Database
         public DbSet<User> Users { get; set; }
         public DbSet<Family> Families { get; set; }
         public DbSet<FamilyMember> FamilyMembers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Budget> Budgets { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UserConfiguration());
             modelBuilder.ApplyConfiguration(new FamilyConfiguration());
             modelBuilder.ApplyConfiguration(new FamilyMemberConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new SubCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new BudgetConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
